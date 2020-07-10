38df3fdce62f448e1a2ac7ab258453c96c4d5aca
Kyle Payne <kspayne93@gmail.com>
2020-07-10 13:41:06 -0600

    initial structure & styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='App'>
+      <div className='header'>
+        <div className='logo-container'>
+          <img src={require('./assets/site-logo.gif')} alt='MobilSense Logo' />
+        </div>
+        <p className='header-welcome-text'>Welcome to MobilSense!</p>
+        <p className='header-bottom-text'>
+          Please follow the instructions to onboard your device onto our network.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className='onboarding-instructions'>
+        <div className='instruction-container'>
+          <div className='instruction-number-container'>
+            <div className='number-circle'>1</div>
+            Click the "Install" button below
+          </div>
+          <div className='instruction-img'>Scrreenshot</div>
+        </div>
+        <div className='instruction-container'>
+          <div className='instruction-number-container'>
+            <div className='number-circle'>2</div>
+            Step 2
+          </div>
+          <div className='instruction-img'>Scrreenshot</div>
+        </div>
+        <div className='instruction-container'>
+          <div className='instruction-number-container'>
+            <div className='number-circle'>3</div>
+            Step 3
+          </div>
+          <div className='instruction-img'>Scrreenshot</div>
+        </div>
+        <div className='instruction-container'>
+          <div className='instruction-number-container'>
+            <div className='number-circle' style={{ paddingRight: '1px' }}>
+              4
+            </div>
+            Step 4
+          </div>
+          <div className='instruction-img'>Scrreenshot</div>
+        </div>
+        <div className='instruction-container'>
+          <div className='instruction-number-container'>
+            <div className='number-circle' style={{ paddingRight: '1px' }}>
+              5
+            </div>
+            Step 5
+          </div>
+          <div className='instruction-img'>Scrreenshot</div>
+        </div>
+
+        <div className='install-button-container'>
+          <button>Install</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
