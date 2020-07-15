061ce6b28b93db59c67983f78fbac8596711dc87
Kyle Payne <kspayne93@gmail.com>
2020-07-15 11:16:14 -0600

    update layout & styling

--- src/App.js
+++ src/App.js
@@ -1,69 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import IOS13Instructions from './components/IOS13Instructions/IOS13Instructions';
+import IOS10Instructions from './components/IOS10Instructions/IOS10Instructions';
+import { getIOSVersion } from './utils.js';
+
+const mainLogo = require('./assets/site-logo.gif');
 
 export default function App() {
+  const [IOSVersion, setIOSVersion] = useState(13);
+  const [downloadComplete, setDownloadComplete] = useState(false);
+  const [id, setId] = useState(null);
+
+  useEffect(() => {
+    const version = getIOSVersion();
+    const id = window.location.href.split('/').pop().split('?')[0];
+
+    if (version) {
+      setIOSVersion(version);
+    }
+    if (id) {
+      setId(id);
+    }
+  }, []);
+
   return (
     <div className='app'>
       <div className='header'>
         <div className='logo-container'>
-          <img src={require('./assets/site-logo.gif')} alt='MobilSense Logo' />
+          <img src={mainLogo} alt='MobilSense Logo' />
         </div>
-        <p className='header-welcome-text'>Welcome to MobilSense!</p>
-        <p className='header-bottom-text'>
-          Please follow the instructions to onboard your device onto our network.
-        </p>
+        {!downloadComplete && (
+          <>
+            <p className='header-welcome-text'>Welcome to MobilSense!</p>
+            <p className='header-bottom-text'>
+              Please follow the instructions to onboard your device onto our network.
+            </p>
+          </>
+        )}
       </div>
 
-      <span className='divider-line'></span>
+      {!downloadComplete ? (
+        <>
+          <span className='divider-line'></span>
 
-      <div className='onboarding-instructions'>
-        <div className='instruction-container'>
-          <div className='instruction-number-container'>
-            <div className='number-circle'>1</div>
-            <p>Click the "Install" button below and some other stuff goes here</p>
-          </div>
-          <div className='instruction-img'>Screenshot</div>
-        </div>
-        <div className='instruction-container'>
-          <div className='instruction-number-container'>
-            <div className='number-circle'>2</div>
-            Step 2
-          </div>
-          <div className='instruction-img'>Screenshot</div>
-        </div>
-        <div className='instruction-container'>
-          <div className='instruction-number-container'>
-            <div className='number-circle'>3</div>
-            Step 3
-          </div>
-          <div className='instruction-img'>Screenshot</div>
-        </div>
-        <div className='instruction-container'>
-          <div className='instruction-number-container'>
-            <div className='number-circle' style={{ paddingRight: '1px' }}>
-              4
-            </div>
-            Step 4
-          </div>
-          <div className='instruction-img'>Screenshot</div>
-        </div>
-        <div className='instruction-container'>
-          <div className='instruction-number-container'>
-            <div className='number-circle' style={{ paddingRight: '1px' }}>
-              5
-            </div>
-            Step 5
-          </div>
-          <div className='instruction-img'>Screenshot</div>
-        </div>
+          {(IOSVersion >= 13 || !IOSVersion) && <IOS13Instructions />}
+          {(IOSVersion < 13 || !IOSVersion) && <IOS10Instructions />}
 
-        <div className='install-button-container'>
-          <p>Click the button below to begin install.</p>
-          {/* <a href='App-prefs://prefs:root=Settings'> */}
-          <button>Install</button>
-          {/* </a> */}
+          <div className='install-button-container'>
+            <p>Click the button below to begin install.</p>
+            <a
+              href={`https://rtsvc.mobilsense.com/api/mobile_config/${id}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button onClick={() => setDownloadComplete(true)}>Install</button>
+            </a>
+          </div>
+        </>
+      ) : (
+        <div>
+          Profile successfully downloaded. Please go to your phone Settings to finish the
+          installation.
+          <a href='App-prefs://prefs:root=Settings'>
+            <button style={{ marginTop: '2rem' }}>Go To Settings</button>
+          </a>
         </div>
-      </div>
+      )}
     </div>
   );
 }
